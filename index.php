125c9ff83f1719c9bd0a6edd213ef3b9056d1469
toto TOTO <toto@toto.toto>
2023-04-04 15:40:08 +0200

    [RDA] ADD index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+        session_start();
+        if(! isset($_SESSION["connected"])) { header("Location: /login.php"); die(); }
+        if($_SESSION["connected"] != 4) { header("Location: /login.php"); die(); }
+?>
+<html>
+        <head>
+                <title>page de test</title>
+        </head>
+        <body>
+                <?php printf("<h1>Hello ".$_SERVER["REMOTE_ADDR"]); ?>
+        </body>
+</html>
